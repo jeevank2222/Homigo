5a8b6bb316cf3df5773654b651e6440df7f697ce
Jeevan <jeevank2782@gmail.com>
2026-08-03 20:07:10 +0530

    integrate Cloudinary for image upload and update

--- controllers/listings.js
+++ controllers/listings.js
@@ -20,8 +20,11 @@ module.exports.showListing = async (req,res)=>{
 };
 
 module.exports.createListing = async(req,res,next)=>{
+    let url = req.file.path;
+    let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {filename,url}
     await newListing.save();
     req.flash("success","New listing created successfully");
     res.redirect("/listings");
@@ -34,12 +37,20 @@ module.exports.renderEditForm = async (req,res)=>{
     req.flash("error","Listing does not exist");
     return res.redirect("/listings");
     }
-    res.render("listings/edit.ejs",{listing})
+    let originalUrl = listing.image.url;
+    originalUrl = originalUrl.replace("/upload","/upload/w_250");
+    res.render("listings/edit.ejs",{listing,originalUrl})
 };
 
 module.exports.updateListing = async (req,res)=>{
     let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    let listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    if(typeof req.file !== "undefined"){
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = {filename,url}
+    await listing.save();
+    }
     req.flash("success","Listing details has been changed successfully");
     res.redirect(`/listings/${id}`);
 };
