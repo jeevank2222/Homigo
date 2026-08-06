d267777dbed9f3638a4e231a7a23615f150731ab
Jeevan <jeevank2782@gmail.com>
2026-08-06 18:57:33 +0530

    final

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./util/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -28,10 +29,11 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-const mongo_url = "mongodb://127.0.0.1:27017/homigo";
+// const mongo_url = "mongodb://127.0.0.1:27017/homigo";
+const dbUrl = process.env.ATLAS_DB_URL;
 
 async function main(){
-    await mongoose.connect(mongo_url);
+    await mongoose.connect(dbUrl);
 }
 
 main()
@@ -39,8 +41,21 @@ main()
     console.log("connected");
 }).catch(err => console.log(err));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret:process.env.SECRET,
+    },
+    touchAfter: 24*3600,
+});
+
+store.on("error",()=>{
+    console.log("error in mongo session store");
+})
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store: store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie:{
